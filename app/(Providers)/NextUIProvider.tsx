2087675c2d0f8cd08bf6dc17206bc1cf8c6982b9
iammarta <108988437+minliamao@users.noreply.github.com>
2024-04-01 10:39:53 +0200

    first commit

---
+++ app/(Providers)/NextUIProvider.tsx
@@ -0,0 +1,16 @@
+'use client'
+import * as React from "react";
+
+// 1. import `NextUIProvider` component
+import {NextUIProvider} from "@nextui-org/react";
+
+interface ProviderProps {
+    children: React.ReactNode;
+}
+
+export default function Provider({children}:ProviderProps) {
+  // 2. Wrap NextUIProvider at the root of your app
+  return (
+    <NextUIProvider>{children}</NextUIProvider>
+  );
+}
