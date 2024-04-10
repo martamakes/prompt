c7f9a41b9c3a3dae9e1228ca897e62a781490e1f
iammarta <108988437+minliamao@users.noreply.github.com>
2024-04-10 21:44:44 +0200

    creating hamburguer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import Link from "next/link";
 import Navigation from "./Navigation";
+import { AiOutlineSearch } from 'react-icons/ai';
+import { CgProfile } from 'react-icons/cg';
+import { FaBars } from 'react-icons/fa';
 
 type Props = {
   activeItem: number;
 }
 
-const Header = ({activeItem}: Props) => {
+const Header = ({ activeItem }: Props) => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
@@ -21,9 +25,8 @@ const Header = ({activeItem}: Props) => {
 
   return (
     <div
-      className={`w-full p-5 border-b min-h-[60px] border-b-[#ffffff32] transition-opacity ${
-        active && "fixed top-0 left-0 bg-[#00000] z-[9999]"
-      }`}
+      className={`w-full p-5 border-b min-h-[60px] border-b-[#ffffff32] transition-opacity ${active && "fixed top-0 left-0 bg-[#00000] z-[9999]"
+        }`}
     >
       <div className="hidden md:w-[90%] mx-auto md:flex items-center justify-between">
         <div>
@@ -33,10 +36,35 @@ const Header = ({activeItem}: Props) => {
             </h1>
           </Link>
         </div>
-     
-      <div className="flex">
-        <Navigation activeItem={activeItem}></Navigation>
+        <div className="flex">
+          <Navigation activeItem={activeItem}>
+          </Navigation>
+        </div>
+        <div className="flex items-center ml-10 ">
+          <AiOutlineSearch className="text-[25px] mr-5 cursor-pointer"></AiOutlineSearch>
+          {/* Todo authentication */}
+          <Link href="/sign-in">
+            <CgProfile className='text-[25px] cursor-pointer' />
+          </Link>
+        </div>
       </div>
+      {/* Todo if logged */}
+      {/* For mobile Screen */}
+      <div className="w-full sm:block md:hidden flex items-center justify-between">
+        <div>
+          <Link href="/">
+            <h1>
+              <Link href={"/"}>
+                <h1 className="font-Inter text-3xl cursor-pointer">
+                  <span className="text-[#64ff4c]">Dis</span>trify
+                </h1>
+              </Link>
+            </h1>
+          </Link>
+        </div>
+        <FaBars className='text-2xl cursor-pointer'
+        onClick={() => setOpen(!open)}
+        />
       </div>
     </div>
   );
